0c0907fbd62325b8678f6269e6954cae1a5dd017
Streak <ahoney@andrew.cmu.edu>
2025-11-04 18:27:54 -0500

    adjusting list spacing, setup for edit/add

---
+++ Recipefy/Recipefy/Views/AddIngredientView.swift
@@ -0,0 +1,18 @@
+//
+//  AddIngredientView.swift
+//  Recipefy
+//
+//  Created by streak honey on 11/4/25.
+//
+
+import SwiftUI
+
+struct AddIngredientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddIngredientView()
+}
---
+++ Recipefy/Recipefy/Views/EditIngredientView.swift
@@ -0,0 +1,18 @@
+//
+//  EditIngredientView.swift
+//  Recipefy
+//
+//  Created by streak honey on 11/4/25.
+//
+
+import SwiftUI
+
+struct EditIngredientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditIngredientView()
+}
