2e815fd1b66d664af124e2b9a683ff17f1b56ada
Jonas <jonass@andrew.cmu.edu>
2025-11-08 13:20:50 -0500

    updated controllers and added recipe and detail views along with tests for the recipe controller and parsing

--- Recipefy/Recipefy/Controllers/RecipeController.swift
+++ Recipefy/Recipefy/Controllers/RecipeController.swift
@@ -11,9 +11,10 @@ import FirebaseFirestore
 import UIKit
 import Combine
 
+@MainActor
 final class RecipeController: ObservableObject {
 	@Published var statusText = "Idle"
-  @Published var currentRecipe: Recipe?
+  @Published var currentRecipes: [Recipe]?
 	@Published var isRetrieving = false
 	@Published var isSaving = false
 	@Published var saveSuccess = false
@@ -23,52 +24,69 @@ final class RecipeController: ObservableObject {
 	
 	func getRecipe(ingredients: [Ingredient]) async {
 		let ingredientsData = ingredients.map { $0.toDictionary() }
+		
+		let formattedIngredients = ingredientsData.compactMap { item in
+					if let name = item["name"], let amount = item["amount"] {
+					 return "\(amount) \(name)"
+			 } else {
+					 return nil
+			 }
+		}
 		isRetrieving = true
-		currentRecipe = nil
+		currentRecipes = nil
 		statusText = "Generating Recipes with AI..."
 		
 		do {
 //			guard
 			
-			let recipe = try await geminiService.getRecipe(ingredients: ingredientsData)
-			currentRecipe = recipe
-			statusText = "Found recipe \(recipe.recipeID)!"
+			let recipe = try await geminiService.getRecipe(ingredients: formattedIngredients)
+			currentRecipes = recipe
+			statusText = "Found \(recipe.count) recipes !"
 			isRetrieving = false
 		} catch {
-			currentRecipe = nil
+			currentRecipes = nil
 			statusText = "Error: \(error.localizedDescription)"
 			isRetrieving = false
 		}
 	}
 	
 	func saveRecipe() async throws {
-		guard let recipe = currentRecipe else {
-			statusText = "No ingredients to save"
+		guard let recipes = currentRecipes else {
+			statusText = "No recipes to save"
 			return
 		}
 		
-		guard let userId = Auth.auth().currentUser?.uid else {
-			statusText = "No authenticated user"
-			return
-		}
+//		guard let userId = Auth.auth().currentUser?.uid else {
+//			statusText = "No authenticated user"
+//			return
+//		}
 		
 		isSaving = true
 		statusText = "Saving to database..."
 		
-		do {
-			let recipeData = recipe.toDictionary()
-			let analysisData: [String: Any] = [
-				"userId": userId,
-				"recipe": recipeData,
-				"analyzedAt": Timestamp(date: Date()),
-				"type": "recipe_generation"
-			]
+		for r in recipes {
+//			saveSuccess = false
 			
-			let docRef = try await db.collection("recipe").addDocument(data: analysisData)
-			statusText = "Saved successfully! (ID: \(docRef.documentID))"
-			saveSuccess = true
-			isSaving = false
+			do {
+				let nutrition = ["carbs": r.carbs, "description": r.description, "fat": r.fat, "fiber": r.fiber, "protein": r.protein] as [String : Any]
+				let analysisData: [String: Any] = [
+	//				"userId": userId,
+					"calories": r.calories,
+					"cookMin": r.cookMin,
+					"createdAt": Timestamp(date: Date()),
+					"ingredients": r.ingredients,
+					"nutrition": nutrition,
+					"servings": r.servings,
+					"steps": r.steps
+				]
+				
+				let docRef = try await db.collection("recipe").addDocument(data: analysisData)
+				statusText = "Saved successfully! (ID: \(docRef.documentID))"
+//				saveSuccess = true
+			}
 		}
+		saveSuccess = true
+		isSaving = false
 	}
 }
 
